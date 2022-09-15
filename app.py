e3941ee214adf527aea3883b85c200635c5c396b
Naftal Rainer <nrainer001@gmail.com>
2022-09-15 18:21:32 +0300

    Style upload section

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import dash
 from dash import Dash, html, dcc 
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
 
 app = Dash(__name__)
+
+# For Bootstrap Icons...
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP]
+)
+# Or for Font Awesome Icons...
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME]
+)
+
 
 
 app.layout = html.Div(className = "content", children=[
@@ -19,14 +30,28 @@ app.layout = html.Div(className = "content", children=[
         ]
 ),
     html.Div(
-        id='sidebar'
-        ),
+        id='sidebar',
+        children=[
+        html.I(className="bi bi-whatsapp"),   
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')]
+            ),
+            multiple=True
+        ) ]
+        
+            
+        ), 
+  
     html.Div(
         id='rightbar'
     ),
     html.Div(
         id='footer'
     )
+    
 ])
 
 
